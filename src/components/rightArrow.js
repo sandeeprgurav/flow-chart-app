b22e0563d044bc46f37e849a3997607c5c56b3d8
Sandeep <sandeeprgurav@gmail.com>
2019-03-12 13:46:31 -0400

    Changes

---
+++ src/components/rightArrow.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class RightArrow extends Component {
+  render() {
+    return (
+      <div className="rightArrow" >
+          →
+      </div>
+    );
+  }
+}
+
+export default RightArrow;
